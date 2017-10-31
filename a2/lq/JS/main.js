054ff1310c39498f27178de13b5cf4003cb1d940
StavrosArgyrou <csd2962@csd.uoc.gr>
2017-10-31 13:43:43 +0200

    Request address using geocode API and handle validity

--- a2/lq/JS/main.js
+++ a2/lq/JS/main.js
@@ -3,6 +3,7 @@
 window.addEventListener('load', function () {
     
     var passwordError = false;
+    //TODO: Maybe refactor this to an implementation without using globals
     var passwordElement = document.getElementById('InputPassword');
     var retypePasswordElement = document.getElementById('InputPassword2');
 
@@ -46,6 +47,48 @@ window.addEventListener('load', function () {
         }
         return false;
     }
+    var addressElement = document.getElementById('InputAddress')
+    addressElement.onblur = function () {
+        var address = addressElement.value;
+        if(address){
+            MakeReq(address);
+        }
+        
+    }
 
+    //todo: move the warning bootstrap classes functionality to the showPasswordFeedback and make that universal
+    function checkAddress(response){
+       if(response.status == "ZERO_RESULTS"){
+        addressElement.classList.add('is-invalid');
+        addressElement.classList.remove('is-valid');
+
+       }else if(response.status == "OK"){
+        addressElement.classList.add('is-valid');
+        addressElement.classList.remove('is-invalid');
+       }else{
+           console.log('Error occurred!');
+       }   
+    }
+
+
+    var btn = document.getElementById('submit');
+    btn.addEventListener('click', MakeReq);
+
+    function MakeReq(address){
+          var xhttp = new XMLHttpRequest();
+        console.log('Response Object created!');
+
+        xhttp.onreadystatechange = function(){
+            if (this.readyState == 4 && this.status == 200) {
+              var responseResult = JSON.parse(this.response);
+             
+              checkAddress(responseResult);
+            }
+        };
+        var url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+address+'&key=AIzaSyB3pGH041-hdat_tpiZtGwK24hfhySA-n4';
+       
+        xhttp.open('GET',url,true);
+        xhttp.send();
+    }
    
 });
