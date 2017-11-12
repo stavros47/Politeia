7402937158f04dc8f2c1823c0f2d2f5168b3b26f
StavrosArgyrou <csd2962@csd.uoc.gr>
2017-11-12 12:15:03 +0200

    Line14: Finally MapWithCallback returns itself so we can have multiple instances of it running at the same time

--- a2/ourMap/ourMap.js
+++ a2/ourMap/ourMap.js
@@ -5,12 +5,12 @@ function ourMap(array) {
     return (function mapWithCallback(fn, callback) {
         // If fn is not a function return current Array
         if (!(fn instanceof Function)) {
-            return current;        }
+            return current; }
         // map fn to array and save result to current array
         current = current.map(fn);
         // If callback is a function, execute callback
         if (callback instanceof Function) {
             callback.call(current, original); }
-        TO COMPLETE! // We have to return something!
-        })
+        return mapWithCallback; // We have to return something!
+    })
 }
