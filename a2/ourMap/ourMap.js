97336122fc6d498634fa4486ba97368180d99e47
StavrosArgyrou <csd2962@csd.uoc.gr>
2017-11-12 12:07:32 +0200

    Line3: We have to track the original array first

---
+++ a2/ourMap/ourMap.js
@@ -0,0 +1,16 @@
+function ourMap(array) {
+    // What do we have to hold/track???
+    var original = array;
+    TO COMPLETE!
+    return (function mapWithCallback(fn, callback) {
+        // If fn is not a function return current Array
+        TO COMPLETE TOGETHER!
+        TO COMPLETE TOGETHER!
+        // map fn to array and save result to current array
+        TO COMPLETE!
+        // If callback is a function, execute callback
+        TO COMPLETE TOGETHER!
+        TO COMPLETE TOGETHER!
+        TO COMPLETE! // We have to return something!
+        })
+}
