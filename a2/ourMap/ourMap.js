2bf802e99c0297a41be00ad49b5ead8bc1fe86b5
StavrosArgyrou <csd2962@csd.uoc.gr>
2017-11-12 12:09:25 +0200

    Lines7-8: Check if fn is a function and return the array if it's not

--- a2/ourMap/ourMap.js
+++ a2/ourMap/ourMap.js
@@ -4,8 +4,8 @@ function ourMap(array) {
     var current = original;
     return (function mapWithCallback(fn, callback) {
         // If fn is not a function return current Array
-        TO COMPLETE TOGETHER!
-        TO COMPLETE TOGETHER!
+        if (!(fn instanceof Function)) {
+            return current;        }
         // map fn to array and save result to current array
         TO COMPLETE!
         // If callback is a function, execute callback
