4641123acb0ecaf7fb51178e998c9a877fe4b939
StavrosArgyrou <csd2962@csd.uoc.gr>
2017-12-01 15:36:51 +0200

    User accessor gets the request and updates the user

--- a3/lq/src/main/java/gr/csd/uoc/cs359/winter2017/lq/model/UserAccessor.java
+++ a3/lq/src/main/java/gr/csd/uoc/cs359/winter2017/lq/model/UserAccessor.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
  *
  * @author Stavr
  */
-public class Registrator {
+public class UserAccessor {
 
     public User RegisterUser(HttpServletRequest request) {
         User newUser = new User();
@@ -33,7 +33,7 @@ public class Registrator {
         String interests = request.getParameter("interests");
         String moreinfo = request.getParameter("moreinfo");
         if (address != null && !address.isEmpty()) {
-            System.out.println("Address:" + address);
+
             newUser.setAddress(address);
         }
         if (interests != null && !interests.isEmpty()) {
@@ -48,17 +48,78 @@ public class Registrator {
         try {
 
             UserDB.addUser(newUser);
-            User test = UserDB.getUser("stavrosargyrou");
-            //
 
-            System.out.println(test.toString());
 
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Registrator.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserAccessor.class.getName()).log(Level.SEVERE, null, ex);
         } catch (Exception ex) {
-            Logger.getLogger(Registrator.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserAccessor.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         return newUser;
     }
+
+    public User updateUserData(HttpServletRequest request, User updatedUser) {
+
+        String email = request.getParameter("email-edit");
+        String address = request.getParameter("address-edit");
+        String interests = request.getParameter("interests-edit");
+        String moreinfo = request.getParameter("moreInfo-edit");
+        String password = request.getParameter("password-edit");
+        String firstname = request.getParameter("firstname-edit");
+        String lastname = request.getParameter("lastname-edit");
+        String gender = request.getParameter("gender");
+        String birthdate = request.getParameter("birthdate-edit");
+        String country = request.getParameter("country");
+        String town = request.getParameter("town-edit");
+        String occupation = request.getParameter("occupation-edit");
+
+        if (email != null && !email.isEmpty()) {
+            updatedUser.setEmail(email);
+        }
+        if (password != null && !password.isEmpty()) {
+            updatedUser.setPassword(password);
+        }
+        if (firstname != null && !firstname.isEmpty()) {
+            updatedUser.setFirstName(firstname);
+        }
+        if (lastname != null && !lastname.isEmpty()) {
+            updatedUser.setLastName(lastname);
+        }
+        if (gender != null && !gender.isEmpty()) {
+            updatedUser.setGender(gender);
+        }
+        if (birthdate != null && !birthdate.isEmpty()) {
+            updatedUser.setBirthDate(birthdate);
+        }
+        if (country != null && !country.isEmpty()) {
+            updatedUser.setCountry(country);
+        }
+        if (town != null && !town.isEmpty()) {
+            updatedUser.setTown(town);
+        }
+        if (occupation != null && !occupation.isEmpty()) {
+            updatedUser.setOccupation(occupation);
+        }
+
+        if (address != null && !address.isEmpty()) {
+            updatedUser.setAddress(address);
+        }
+        if (interests != null && !interests.isEmpty()) {
+
+            updatedUser.setInterests(interests);
+        }
+        if (moreinfo != null && !moreinfo.isEmpty()) {
+            updatedUser.setInfo(moreinfo);
+        }
+
+        try {
+            UserDB.updateUser(updatedUser);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UserAccessor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return updatedUser;
+    }
+
 }
