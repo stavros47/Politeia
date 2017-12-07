cafd3e3fa88ca7c159acf5d4b4860f0a1b5f2c9e
iakovou <iakovou@csd.uoc.gr>
2017-12-07 12:33:51 +0200

    return to previous working version

--- a4/lq/src/main/java/gr/csd/uoc/cs359/winter2017/lq/model/PollAccessor.java
+++ a4/lq/src/main/java/gr/csd/uoc/cs359/winter2017/lq/model/PollAccessor.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gr.csd.uoc.cs359.winter2017.lq.model;
 
 import gr.csd.uoc.cs359.winter2017.lq.db.InitiativeDB;
