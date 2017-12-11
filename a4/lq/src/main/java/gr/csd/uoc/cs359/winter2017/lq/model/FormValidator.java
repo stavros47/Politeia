f08971bca25a4d9a6a9231849e6404c467a4f094
iakovou <iakovou@csd.uoc.gr>
2017-12-11 13:12:43 +0200

    Bug fixes

--- a4/lq/src/main/java/gr/csd/uoc/cs359/winter2017/lq/model/FormValidator.java
+++ a4/lq/src/main/java/gr/csd/uoc/cs359/winter2017/lq/model/FormValidator.java
@@ -22,10 +22,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class FormValidator {
 
-    private HashMap<String, String> fieldRegexMap = new HashMap<>();
-    private HashMap<String, String> updateFieldRegexMap = new HashMap<>();
-    private HashMap<String, String> optionalFieldRegexMap = new HashMap<>();
-    private ArrayList<String> invalidFields = new ArrayList<>();
+    private HashMap < String, String > fieldRegexMap = new HashMap < > ();
+    private HashMap < String, String > updateFieldRegexMap = new HashMap < > ();
+    private HashMap < String, String > optionalFieldRegexMap = new HashMap < > ();
+    private ArrayList < String > invalidFields = new ArrayList < > ();
 
     public FormValidator() {
         //Registration Fields
@@ -61,7 +61,7 @@ public class FormValidator {
 
     public void ValidateRequired(HttpServletRequest request) {
         //Get all parameters from the request and validate them key:parametername value:regex
-        for (Map.Entry<String, String> entry : this.fieldRegexMap.entrySet()) {
+        for (Map.Entry < String, String > entry: this.fieldRegexMap.entrySet()) {
             //Check if the parameter exists
             if (request.getParameter(entry.getKey()) != null && (!request.getParameter(entry.getKey()).isEmpty())) {
                 //Patern to check gets added in the Pattern object
@@ -80,7 +80,7 @@ public class FormValidator {
     public void ValidateOptional(HttpServletRequest request) {
 
         //Get all parameters from the request and validate them key:parametername value:regex
-        for (Map.Entry<String, String> entry : this.optionalFieldRegexMap.entrySet()) {
+        for (Map.Entry < String, String > entry: this.optionalFieldRegexMap.entrySet()) {
             //Check if the parameter exists
             if (request.getParameter(entry.getKey()) != null && (!request.getParameter(entry.getKey()).isEmpty())) {
                 //Patern to check gets added in the Pattern object
@@ -94,14 +94,14 @@ public class FormValidator {
 
     }
 
-    public ArrayList<String> Validate(HttpServletRequest request) {
+    public ArrayList < String > Validate(HttpServletRequest request) {
         ValidateOptional(request);
         ValidateRequired(request);
 
         return this.invalidFields;
     }
 
-    public ArrayList<String> ValidateLogin(HttpServletRequest request) {
+    public ArrayList < String > ValidateLogin(HttpServletRequest request) {
         String username = (String) request.getParameter("lgnUsername");
         String password = (String) request.getParameter("lgnPassword");
         String usernameRegex = ".{8,}";
@@ -128,7 +128,7 @@ public class FormValidator {
     }
 
     // 0: username 1: email
-    public ArrayList<String> checkDuplicate(String field, int flag, String name) {
+    public ArrayList < String > checkDuplicate(String field, int flag, String name) {
 
         try {
             if (flag == 0) {
@@ -148,9 +148,9 @@ public class FormValidator {
         return this.invalidFields;
     }
 
-    public ArrayList<String> ValidateUpdatedFields(HttpServletRequest request) {
+    public ArrayList < String > ValidateUpdatedFields(HttpServletRequest request) {
         //Get all parameters from the request and validate them key:parametername value:regex
-        for (Map.Entry<String, String> entry : this.updateFieldRegexMap.entrySet()) {
+        for (Map.Entry < String, String > entry: this.updateFieldRegexMap.entrySet()) {
             //Check if the parameter exists
             if (request.getParameter(entry.getKey()) != null && (!request.getParameter(entry.getKey()).isEmpty())) {
                 //Patern to check gets added in the Pattern object
@@ -170,80 +170,79 @@ public class FormValidator {
      * @return
      * @throws java.text.ParseException
      */
-    public ArrayList<String> ValidatePollFields (HttpServletRequest request) throws ParseException{
-         Date today = new Date();
-         String description = (String) request.getParameter("description-newPolicy"); 
-         String category =(String) request.getParameter("category-newPolicy");
-         String title = (String) request.getParameter("title-newPolicy");
-         String expDate= (String)request.getParameter("expiration-newPolicy");
-         String status = (String) request.getParameter("status-newPolicy");
-         String time = (String) request.getParameter("expTime-newPolicy");
-         String timeregex= "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"; 
-         String dateTime= expDate + " " + time; 
-        if (description == null || description.trim().isEmpty()){
-                 this.invalidFields.add("Emptydescription-newPolicy");
-             }
-         if (category == null || category.trim().isEmpty()){
-                 this.invalidFields.add("Emptycategory-newPolicy");
-         }
-        if (title == null || title.trim().isEmpty()){
-                 this.invalidFields.add("Emptytitle-newPolicy");
+    public ArrayList < String > ValidatePollFields(HttpServletRequest request) throws ParseException {
+        Date today = new Date();
+        String description = (String) request.getParameter("description-newPolicy");
+        String category = (String) request.getParameter("category-newPolicy");
+        String title = (String) request.getParameter("title-newPolicy");
+        String expDate = (String) request.getParameter("expiration-newPolicy");
+        String status = (String) request.getParameter("status-newPolicy");
+        String time = (String) request.getParameter("expTime-newPolicy");
+        String timeregex = "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+        String dateTime = expDate + " " + time;
+        if (description == null || description.trim().isEmpty()) {
+            this.invalidFields.add("Emptydescription-newPolicy");
         }
-        if (status.equals("1")){
-             if (time == null || time.trim().isEmpty()) {
-                 this.invalidFields.add("EmptyexpTime-newPolicy");
-             } 
-             else {
-                 if (!time.matches(timeregex)) {
+        if (category == null || category.trim().isEmpty()) {
+            this.invalidFields.add("Emptycategory-newPolicy");
+        }
+        if (title == null || title.trim().isEmpty()) {
+            this.invalidFields.add("Emptytitle-newPolicy");
+        }
+        if (status.equals("1")) {
+            if (time == null || time.trim().isEmpty()) {
+                this.invalidFields.add("EmptyexpTime-newPolicy");
+            } else {
+                if (!time.matches(timeregex)) {
                     this.invalidFields.add("expTime-newPolicy");
-                  }
+                }
             }
             if (expDate == null || expDate.trim().isEmpty()) {
                 this.invalidFields.add("expiration-newPolicy");
             } else {
                 Date expiration = (Date) new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateTime);
-                
-                if (expiration.getTime()< today.getTime()) {
+
+                if (expiration.getTime() < today.getTime()) {
                     this.invalidFields.add("badDateexpiration-newPolicy");
                 }
             }
         }
         return this.invalidFields;
     }
-       
-    
-    
-    
-    public ArrayList<String> ValidatePollEditFields (HttpServletRequest request) throws ParseException{
-         Date today = new Date();
-         String time =(String) request.getParameter("expTime-editPolicy");
-         String status = (String) request.getParameter("status-editPolicy");
-         String expDate= (String)request.getParameter("expiration-editPolicy");
-         String timeregex= "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$"; 
-         String dateTime= expDate + " " + time; 
-           
-        if (status.equals("1")){
-             if (time == null || time.trim().isEmpty()) {
-                 this.invalidFields.add("EmptyexpTime-editPolicy");
-             } 
-             else {
-                 if (!time.matches(timeregex)) {
+
+
+
+
+    public ArrayList < String > ValidatePollEditFields(HttpServletRequest request) throws ParseException {
+        Date today = new Date();
+        String time = (String) request.getParameter("expTime-editPolicy");
+        String status = (String) request.getParameter("status-editPolicy");
+        String expDate = (String) request.getParameter("expiration-editPolicy");
+        String timeregex = "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$";
+        String dateTime = expDate + " " + time;
+
+        if (status.equals("1")) {
+            if (time == null || time.trim().isEmpty()) {
+                this.invalidFields.add("EmptyexpTime-editPolicy");
+            } 
+            else {
+                if (!time.matches(timeregex)) {
                     this.invalidFields.add("expTime-editPolicy");
-                  }
-            }
-            if (expDate == null || expDate.trim().isEmpty()) {
-                this.invalidFields.add("expiration-editPolicy");
-            } else {
-                Date expiration = (Date) new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateTime);
-                
-                if (expiration.getTime()< today.getTime()) {
-                    this.invalidFields.add("badDateexpiration-editPolicy");
+                } else {
+
+                    if (expDate==null || expDate.trim().isEmpty()) {
+                        this.invalidFields.add("expiration-editPolicy");
+                    } else {
+                        Date expiration = (Date) new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dateTime);
+
+                        if (expiration.getTime() < today.getTime()) {
+                            this.invalidFields.add("badDateexpiration-editPolicy");
+                        }
+                    }
                 }
             }
-         
-       
         }
         return this.invalidFields;
     }
-    
+
 }
